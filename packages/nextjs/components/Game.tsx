36a37a52f389eae29eac193bc286a4f3b8d404bd
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-05-03 15:50:01 +0100

    rename

--- packages/nextjs/components/Game.tsx
+++ packages/nextjs/components/Game.tsx
@@ -320,7 +320,7 @@ export const Game: React.FC = ({}) => {
                                             setModalOpenId("submit-build-modal");
                                           }}
                                         >
-                                          Submit Build
+                                          Build & Join
                                         </button>
                                       </td>
                                     </tr>
