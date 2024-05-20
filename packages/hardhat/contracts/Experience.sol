93995b70827c04deae659941a45ae8a5c1383efd
Dhvani Patel <dhvani.patel@uwaterloo.ca>
2024-05-20 18:59:44 +0200

    test contract

--- packages/hardhat/contracts/Experience.sol
+++ packages/hardhat/contracts/Experience.sol
@@ -1,47 +1,40 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.24;
 
-import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/governance/Governor.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorSettings.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
+import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
+
 import { StoreSwitch } from "@latticexyz/store/src/StoreSwitch.sol";
-import { ResourceId, WorldResourceIdLib, WorldResourceIdInstance } from "@latticexyz/world/src/WorldResourceId.sol";
-import { Hook } from "@latticexyz/store/src/Hook.sol";
-import { IERC165 } from "@latticexyz/world/src/IERC165.sol";
-import { ICustomUnregisterDelegation } from "@latticexyz/world/src/ICustomUnregisterDelegation.sol";
-import { IOptionalSystemHook } from "@latticexyz/world/src/IOptionalSystemHook.sol";
-import { BEFORE_CALL_SYSTEM, AFTER_CALL_SYSTEM, ALL } from "@latticexyz/world/src/systemHookTypes.sol";
-import { RESOURCE_SYSTEM } from "@latticexyz/world/src/worldResourceTypes.sol";
-import { OptionalSystemHooks } from "@latticexyz/world/src/codegen/tables/OptionalSystemHooks.sol";
+import { ResourceId } from "@latticexyz/world/src/WorldResourceId.sol";
+import { ISystemHook } from "@latticexyz/world/src/ISystemHook.sol";
 
-import { IWorld } from "@biomesaw/world/src/codegen/world/IWorld.sol";
-import { VoxelCoord } from "@biomesaw/utils/src/Types.sol";
-import { voxelCoordsAreEqual, inSurroundingCube } from "@biomesaw/utils/src/VoxelCoordUtils.sol";
-
-// Available utils, remove the ones you don't need
-// See ObjectTypeIds.sol for all available object types
-import { PlayerObjectID, AirObjectID, DirtObjectID, ChestObjectID } from "@biomesaw/world/src/ObjectTypeIds.sol";
-import { getBuildArgs, getMineArgs, getMoveArgs, getHitArgs, getDropArgs, getTransferArgs, getCraftArgs, getEquipArgs, getLoginArgs, getSpawnArgs } from "../utils/HookUtils.sol";
-import { getSystemId, isSystemId, callBuild, callMine, callMove, callHit, callDrop, callTransfer, callCraft, callEquip, callUnequip, callLogin, callLogout, callSpawn, callActivate } from "../utils/DelegationUtils.sol";
-import { hasBeforeAndAfterSystemHook, getObjectTypeAtCoord, getEntityAtCoord, getPosition, getObjectType, getMiningDifficulty, getStackable, getDamage, getDurability, isTool, isBlock, getEntityFromPlayer, getPlayerFromEntity, getEquipped, getHealth, getStamina, getIsLoggedOff, getLastHitTime, getInventoryTool, getInventoryObjects, getCount, getNumSlotsUsed, getNumUsesLeft } from "../utils/EntityUtils.sol";
-import { Area, insideArea, insideAreaIgnoreY, getEntitiesInArea } from "../utils/AreaUtils.sol";
-import { Build, BuildWithPos, buildExistsInWorld, buildWithPosExistsInWorld } from "../utils/BuildUtils.sol";
-import { NamedArea, NamedBuild, NamedBuildWithPos, weiToString, getEmptyBlockOnGround } from "../utils/GameUtils.sol";
-
-contract Experience is ICustomUnregisterDelegation, IOptionalSystemHook {
+contract Experience is
+  ISystemHook,
+  Governor,
+  GovernorSettings,
+  GovernorCountingSimple,
+  GovernorVotes,
+  GovernorVotesQuorumFraction
+{
   address public immutable biomeWorldAddress;
 
-  address public delegatorAddress;
-
-  // Event to show a notification in the Biomes World
-  event GameNotif(address player, string message);
-
-  constructor(address _biomeWorldAddress, address _delegatorAddress) {
+  constructor(
+    address _biomeWorldAddress,
+    IVotes _token
+  )
+    Governor("MyGovernor")
+    GovernorSettings(43200 /* 1 day */, 302400 /* 1 week */, 0)
+    GovernorVotes(_token)
+    GovernorVotesQuorumFraction(4)
+  {
     biomeWorldAddress = _biomeWorldAddress;
 
     // Set the store address, so that when reading from MUD tables in the
     // Biomes world, we don't need to pass the store address every time.
     StoreSwitch.setStoreAddress(_biomeWorldAddress);
-
-    delegatorAddress = _delegatorAddress;
   }
 
   // Use this modifier to restrict access to the Biomes World contract only
@@ -51,30 +44,9 @@ contract Experience is ICustomUnregisterDelegation, IOptionalSystemHook {
     _; // Continue execution
   }
 
-  function supportsInterface(bytes4 interfaceId) external view override returns (bool) {
-    return
-      interfaceId == type(ICustomUnregisterDelegation).interfaceId ||
-      interfaceId == type(IOptionalSystemHook).interfaceId ||
-      interfaceId == type(IERC165).interfaceId;
+  function supportsInterface(bytes4 interfaceId) public view override(IERC165, Governor) returns (bool) {
+    return interfaceId == type(ISystemHook).interfaceId || super.supportsInterface(interfaceId);
   }
-
-  function canUnregister(address delegator) external override onlyBiomeWorld returns (bool) {
-    return true;
-  }
-
-  function onRegisterHook(
-    address msgSender,
-    ResourceId systemId,
-    uint8 enabledHooksBitmap,
-    bytes32 callDataHash
-  ) external override onlyBiomeWorld {}
-
-  function onUnregisterHook(
-    address msgSender,
-    ResourceId systemId,
-    uint8 enabledHooksBitmap,
-    bytes32 callDataHash
-  ) external override onlyBiomeWorld {}
 
   function onBeforeCallSystem(
     address msgSender,
@@ -88,19 +60,20 @@ contract Experience is ICustomUnregisterDelegation, IOptionalSystemHook {
     bytes memory callData
   ) external override onlyBiomeWorld {}
 
-  function basicGetter() external view returns (uint256) {
-    return 42;
+  // The following functions are overrides required by Solidity.
+  function votingDelay() public view override(Governor, GovernorSettings) returns (uint256) {
+    return super.votingDelay();
   }
 
-  function getRegisteredPlayers() external view returns (address[] memory) {
-    return new address[](0);
+  function votingPeriod() public view override(Governor, GovernorSettings) returns (uint256) {
+    return super.votingPeriod();
   }
 
-  function getDisplayName() external view returns (string memory) {
-    return "Experience";
+  function quorum(uint256 blockNumber) public view override(Governor, GovernorVotesQuorumFraction) returns (uint256) {
+    return super.quorum(blockNumber);
   }
 
-  function getStatus() external view returns (string memory) {
-    return "You are in the Experience";
+  function proposalThreshold() public view override(Governor, GovernorSettings) returns (uint256) {
+    return super.proposalThreshold();
   }
 }
